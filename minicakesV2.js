5272fc8b3df4adf18cf9a5cfdb06077f5782cc6e
anget92 <69660112+anget92@users.noreply.github.com>
2021-06-01 14:20:53 -0300

    Actualizacion_minicakes

---
+++ minicakesV2.js
@@ -0,0 +1,72 @@
+let images=["AT-tienda-img/cake-nut.jpeg", "AT-tienda-img/cake-mariposas.jpg", "AT-tienda-img/cake-marbleblue.jpeg", "AT-tienda-img/cake-shag.jpg", "AT-tienda-img/cake-sprinkles.jpg"]
+
+let slider=document.querySelector("#slides-js");
+
+slider.style.width = images.length * 100 + "%"
+
+for(let img in images){
+
+    slider.innerHTML += `<div class="slider_content" style= "width:${100/images.length}%"><img src="${images[img]}" class="slide_img"></div>`
+}
+
+
+let sliderContent=document.querySelectorAll(".slider_content");
+
+
+
+let imagesLast = sliderContent[sliderContent.length -1]; 
+
+slider.insertAdjacentElement("afterbegin", imagesLast);
+
+const btnLeft = document.querySelector('#btn-left')
+const btnRight = document.querySelector('#btn-right')
+
+function Next() {
+    let imageFirst = document.querySelectorAll(".slider_content")[0]
+    slider.style.marginLeft = "-200%"
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none"
+        slider.insertAdjacentElement("beforeend", imageFirst)
+        slider.style.marginLeft = "-100%"
+    }, 500)
+}
+
+
+function Prev() {
+    let sliderContent = document.querySelectorAll(".slider_content")
+    let imagesLast = sliderContent[sliderContent.length -1]
+    slider.style.marginLeft = "1px"
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none"
+        slider.insertAdjacentElement("afterbegin", imagesLast)
+        slider.style.marginLeft = "-100%"
+    }, 500)
+}
+
+btnRight.addEventListener("click", function(){
+    Next()
+})
+
+btnLeft.addEventListener("click", function(){
+    Prev()
+})
+
+setInterval(function(){
+    Next()
+}, 5000)
+
+
+function selectOption(){
+
+    let opciones = document.getElementById("select")
+    opcion= (opciones.value)
+    document.getElementById("opcionSeleccionada").innerText = `${opcion}`
+
+}
+
+
+
+
+
